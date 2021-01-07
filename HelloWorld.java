719eafd1ea9e331ced813d915adf6e198e0124d4
kannan <ggkannan1@gmail.com>
2021-01-07 19:50:57 +0000

    added poll scm line

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, Welcome to Devops!");
     System.out.println("We are exploring Jenkins");
+    System.out.println("We are exploring Build Triggers-Poll SCM");
   }
 }
