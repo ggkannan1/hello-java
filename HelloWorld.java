3a7e28751b5400a8242fdc4b227163b993eae9da
kannan <ggkannan1@gmail.com>
2021-01-07 12:18:57 +0000

    added new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, Welcome to Devops-Jenkins");
+    System.out.println("Hello, Welcome to Devops!");
+    System.out.println("We are exploring Jenkins");
   }
 }
